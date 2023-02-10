c5e9ec62292f6096f1f5c3f16713218715f347e2
chen.fei <chenfei@qq.com>
2023-02-10 14:34:28 +0800

    prepare

--- source/ui/MainWindow.cpp
+++ source/ui/MainWindow.cpp
@@ -1,7 +1,17 @@
-#include "MainWindow.h"
+﻿#include "MainWindow.h"
+#include "MainpageWidget.h"
+#include "settingWidget.h"
+#include <QHBoxLayout>
+#include <QTabWidget>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+	// mainPage
+	ui.tabWidget->addTab(new MainPageWidget, style()->standardIcon(QStyle::SP_DesktopIcon), tr(u8"主页"));
+
+	// SettingWidget
+	ui.tabWidget->addTab(new SettingWidget, style()->standardIcon(QStyle::SP_CommandLink), tr(u8"设置"));
 }
