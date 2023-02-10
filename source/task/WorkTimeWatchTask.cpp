c6a932e4e028deb189de486e5a352a7ecbec87ca
chen.fei <chenfei@qq.com>
2023-02-10 15:52:29 +0800

    prepare

--- source/task/WorkTimeWatchTask.cpp
+++ source/task/WorkTimeWatchTask.cpp
@@ -13,24 +13,29 @@
 *   See the License for the specific language governing permissions and
 *   limitations under the License.
 */
+#include "WorkTimeWatchTask.h"
 
-#include "MainWindow.h"
-#include "MainpageWidget.h"
-#include "settingWidget.h"
-#include <QHBoxLayout>
-#include <QTabWidget>
-
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+WorkTimeWatchTask::WorkTimeWatchTask()
 {
-    ui.setupUi(this);
 
-	// mainPage
-	ui.tabWidget->addTab(new MainPageWidget, style()->standardIcon(QStyle::SP_DesktopIcon), tr(u8"主页"));
+}
 
-	// SettingWidget
-	ui.tabWidget->addTab(new SettingWidget, style()->standardIcon(QStyle::SP_CommandLink), tr(u8"设置"));
+bool WorkTimeWatchTask::Run()
+{
+	if (runFlag_)
+	{
+		return true;
+	}
 
-	// backGround
-	setStyleSheet("MainWindow:{background::}");
+	return false;
+}
+
+bool WorkTimeWatchTask::Stop()
+{
+	if (!runFlag_)
+	{
+		return true;
+	}
+
+	return false;
 }
--- source/task/WorkTimeWatchTask.h
+++ source/task/WorkTimeWatchTask.h
@@ -14,23 +14,13 @@
 *   limitations under the License.
 */
 
-#include "MainWindow.h"
-#include "MainpageWidget.h"
-#include "settingWidget.h"
-#include <QHBoxLayout>
-#include <QTabWidget>
+#pragma once
+#include "Task.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+class WorkTimeWatchTask : virtual public Task
 {
-    ui.setupUi(this);
-
-	// mainPage
-	ui.tabWidget->addTab(new MainPageWidget, style()->standardIcon(QStyle::SP_DesktopIcon), tr(u8"主页"));
-
-	// SettingWidget
-	ui.tabWidget->addTab(new SettingWidget, style()->standardIcon(QStyle::SP_CommandLink), tr(u8"设置"));
-
-	// backGround
-	setStyleSheet("MainWindow:{background::}");
-}
+public:
+	WorkTimeWatchTask();
+	virtual bool Run() override;
+	virtual bool Stop() override;
+};
